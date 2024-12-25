6c493d5b14103235977faabb5b7f567a1b85e610
Christian <ChrisThomas0634@gmail.com>
2024-12-24 21:06:20 -0500

    Update 12/24/24

--- login.js
+++ login.js
@@ -2,25 +2,56 @@ class user{
                 // Variables //
     usernameFromInput = document.getElementById("Username");
     passwordFromInput = document.getElementById("Password");
-    isAdministrator;
+    userId;
     static totalUsers;
 
 
 
                 // Constructor //
-    constructor(username, password, admin){
+    constructor(username, password, ID){
         this.usernameFromInput = username;
         this.passwordFromInput = password;
-        this.isAdministrator = admin;
+        this.userId = ID;
+
         totalUsers++;
 
                 // Methods //
 
     }
-
-
     
+}
+
+//parent class //
+
+
+
+
+class admin extends user{
+
+    constructor(username, password, ID, isAdmin){
+       super(username, password, ID);
+       this.isAdmin = isAdmin;
+       totalUsers++;
+}
+
+
 
 
 
 }
+
+// subclass for admins //
+
+class deactivated extends user{
+
+    constructor(username, password, ID, isForceDeactivated, isTemporarilyDeactivated){
+       super(username, password, ID);
+       this.isForceDeactivated = isForceDeactivated;
+       this.isTemporarilyDeactivated = isTemporarilyDeactivated;
+       totalUsers++;
+}
+}
+
+
+
+
